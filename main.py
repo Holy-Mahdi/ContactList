bca545e8c9c53d2143f4c7bf115ba3bbf155e219
Mahdi Mohammadi <mohammadimahdiofficial@gmail.com>
2025-08-31 15:52:11 +0330

    Add add_contact function to enable adding new contacts

--- main.py
+++ main.py
@@ -15,3 +15,10 @@ def save_contacts(contacts):
     if os.path.exists(FILE_NAME):
         with open(FILE_NAME,"w") as f:
             json.dump(contacts,f,indent=4)
+
+
+def add_contact(contacts):
+    name = str(input("contact name : "))
+    phone = str(input("contact number : "))
+    contacts[name] = phone
+    print("Contact added.")
