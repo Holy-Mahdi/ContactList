c3515d58eae7fa57b249d95973538acfce729745
Mahdi Mohammadi <mohammadimahdiofficial@gmail.com>
2025-08-31 15:49:08 +0330

    Implement save_contacts function to persist contact data

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def load_contacts():
             return json.load(f)
     
     return {}
+
+
+def save_contacts(contacts):
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME,"w") as f:
+            json.dump(contacts,f,indent=4)
